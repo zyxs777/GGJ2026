47f67f554e06aab5ad6eade2b850e8b06d1ff1ff
spathering <2607758648@qq.com>
2026-01-30 23:36:26 +0800

    玩家移动控制器

--- Assets/Scripts/Global/GameMgr.cs
+++ Assets/Scripts/Global/GameMgr.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Global
@@ -10,6 +11,30 @@ namespace Global
             GlobalShare.Reset();
 
         }
+
         
+        private void Awake()
+        {
+            Physics.simulationMode = SimulationMode.Script;
+            GlobalShare.GlobalTime.OnValueChanged = OnTimeScaled;
+            _timeDelta = Time.fixedDeltaTime;
+        }
+        private void FixedUpdate()
+        {
+            UpdateTime();
+        }
+
+        #region Time Management
+        private float _timeDelta;
+        private void OnTimeScaled(float scale)
+        {
+            _timeDelta = Time.fixedDeltaTime * scale;
+        }
+
+        private void UpdateTime()
+        {
+            Physics.Simulate(_timeDelta);
+        }
+        #endregion
     }
 }
--- Assets/Scripts/Player/PlayerMotion.cs
+++ Assets/Scripts/Player/PlayerMotion.cs
@@ -1,10 +1,29 @@
 using System;
+using Global;
+using Sirenix.OdinInspector;
+using STool;
+using STool.CollectionUtility;
 using UnityEngine;
 
 namespace Player
 {
     public sealed class PlayerMotion : MonoBehaviour
     {
+        #region Player Setting
+
+        [FoldoutGroup("PlayerSetting")] [SerializeField]
+        private int playerIndex = 0;
+
+        #endregion
+        
+        #region Mono
+        [FoldoutGroup("Components")] [SerializeReference] private Rigidbody rig;
+
+        private void Awake()
+        {
+            InitMotionDecorator();
+        }
+
         private void OnEnable()
         {
             RegisterCtrl();
@@ -15,20 +34,99 @@ namespace Player
             UnRegisterCtrl();
         }
 
+        private void Update()
+        {
+            GetInput();
+        }
+
+        private void FixedUpdate()
+        {
+            DoImpulseDecline();
+            DealVelocity();
+        }
+
+        #region Collision
+
+        private void OnCollisionEnter(Collision other)
+        {
+            _impulseVelocity += other.impulse;
+        }
+        #endregion
+        #endregion
 
         #region Rewired Registration
+        private Rewired.Player _player;
         private void RegisterCtrl()
         {
-            var player = Rewired.ReInput.players.GetPlayer(0);
+            _player ??= Rewired.ReInput.players.GetPlayer(playerIndex);
             // player.AddInputEventDelegate();
         }
 
         private void UnRegisterCtrl()
         {
-            var player = Rewired.ReInput.players.GetPlayer(0);
-            
+            _player ??= Rewired.ReInput.players.GetPlayer(playerIndex);
         }
 
+        #endregion
+
+        #region Motion
+        [FoldoutGroup("Motion")] [SerializeField] private float inputAcceleration = 10;
+        [FoldoutGroup("Motion")] [SerializeField] private float inputSpeedMaximum = 5;
+        [FoldoutGroup("Motion")] [SerializeField] private float impulseDeclineValue = 5;
+        [FoldoutGroup("Motion")] [SerializeField] private float jumpVelocity = 10;
+        
+        private DecoratedValue<Vector3> _motionCalculation;
+        private DecoratedValue<Vector3>.ModifierCollectionToken _inputToken;
+        private DecoratedValue<Vector3>.ModifierCollectionToken _impulseToken;
+        private Vector3 _impulseVelocity;
+        private Vector3 Vec3Add(Vector3 a, Vector3 b) => a + b;
+        private void InitMotionDecorator()
+        {
+            _motionCalculation ??= new(Vector3.zero);
+            _inputToken = _motionCalculation.Add(Vector3.zero, Vec3Add);
+            _impulseToken = _motionCalculation.Add(Vector3.zero, Vec3Add);
+        }
+
+        #region Proactive
+        [FoldoutGroup("Motion")] [ShowInInspector] [ReadOnly] private Vector2 _velocityInput;
+        [FoldoutGroup("Motion")] [ShowInInspector] [ReadOnly] private Vector2 _velocityProactive;
+        private void GetInput()
+        {
+            _velocityInput = Vector2.ClampMagnitude(_player.GetAxis2D("moveX", "moveY"), 1);
+            var velInputChange = _velocityInput - _velocityProactive;
+            velInputChange = Vector2.ClampMagnitude(velInputChange, GlobalShare.GlobalTimeDelta);
+            _velocityProactive += velInputChange;
+            //主动速度加减速
+            _inputToken.SetValue( inputSpeedMaximum * _velocityProactive.ConvertXZ(), false);
+        }
+
+        #endregion
+
+        #region Impulse
+        private void DoImpulseDecline()
+        {
+            var dir = _impulseVelocity.normalized;
+            var mag = Mathf.Max(0, dir.magnitude - impulseDeclineValue * GlobalShare.GlobalTimeDelta);
+            var impRes = mag * dir;
+
+            _impulseToken.SetValue(impRes, false);
+            _impulseVelocity = impRes;
+        }
+        #endregion
+        
+        private void DealVelocity()
+        {
+            _motionCalculation.Recompute();
+            var output = _motionCalculation.Value;
+         
+            rig.velocity = output;
+            // var outputXZ = new Vector3(output.x, 0, output.z);
+            // var velOfRig = rig.velocity;
+            
+            // velOfRig.x =  outputXZ.x;
+            // velOfRig.z = outputXZ.z;
+            // rig.velocity = velOfRig;
+        }
         #endregion
     }
 }
