c85185545b290b2f81288e6dd9b1e043d5905d37
spathering <2607758648@qq.com>
2026-01-31 19:47:17 +0800

    更新关卡切换时的黑幕过度UI

--- Assets/Scripts/Global/GlobalEvent.cs
+++ Assets/Scripts/Global/GlobalEvent.cs
@@ -2,23 +2,10 @@ using UnityEngine;
 
 namespace Global
 {
-    public struct Global_EnterLevel
-    {
-        
-    }
+    public struct Global_EnterLevel { }
+    public struct Global_EndLevel{}
+    public struct Global_ExitLevel { }
 
-    public struct Global_ExitLevel
-    {
-        
-    }
-
-    public struct Global_GamePause
-    {
-        
-    }
-
-    public struct Global_GameResume
-    {
-        
-    }
+    public struct Global_GamePause { }
+    public struct Global_GameResume { }
 }
